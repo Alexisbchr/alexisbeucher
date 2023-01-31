9408f57d662b770f7303d1a96ff91948fbcd8eb4
alexisbchr <alexis.beucher01@gmail.com>
2023-01-31 21:41:12 +0100

    UPDATE BLOG FUNCTION

--- assets/managers/BlogManager.php
+++ assets/managers/BlogManager.php
@@ -2,111 +2,110 @@
 
 class BlogManager extends AbstractManager
 {
-    public function getAllArticles() : array
+	public function getAllArticles(): array
 	{
-        $db=$this->db;
-        $query = $db->prepare('SELECT * FROM blog');
-        $query->execute();
-        $articles = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $articles;
+		$db = $this->db;
+		$query = $db->prepare('SELECT * FROM blog ORDER BY blog.id DESC');
+		$query->execute();
+		$articles = $query->fetchAll(PDO::FETCH_ASSOC);
+		return $articles;
 	}
-    public function getArticlesById(int $id) : array
+	public function getArticlesById(int $id): array
 	{
-        $db=$this->db;
-        $query = $db->prepare('SELECT * FROM blog WHERE blog.id=:id');
-        $parameters = [
-        'id' => $id
-        ];
-        $query->execute($parameters);
-        $articles = $query->fetchAll(PDO::FETCH_ASSOC);
-        return $articles;
-	} 
-    public function getUrlByImageId(int $image_id) : string
-	{
-        $db=$this->db;
-        $query = $db->prepare('SELECT url FROM image_blog WHERE image_blog.id=:image_id');
-        $parameters = [
-        'image_id' => $image_id
-        ];
-        $query->execute($parameters);
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-        $url = $result['url'];
-        return $url;
+		$db = $this->db;
+		$query = $db->prepare('SELECT * FROM blog WHERE blog.id=:id');
+		$parameters = [
+			'id' => $id
+		];
+		$query->execute($parameters);
+		$articles = $query->fetchAll(PDO::FETCH_ASSOC);
+		return $articles;
 	}
-    public function getAltByImageId(int $image_id) : string
+	public function getUrlByImageId(int $image_id): string
 	{
-        $db=$this->db;
-        $query = $db->prepare('SELECT alt FROM image_blog WHERE image_blog.id=:image_id');
-        $parameters = [
-        'image_id' => $image_id
-        ];
-        $query->execute($parameters);
-        $result = $query->fetch(PDO::FETCH_ASSOC);
-        $alt = $result['alt'];
-        return $alt;
+		$db = $this->db;
+		$query = $db->prepare('SELECT url FROM image_blog WHERE image_blog.id=:image_id');
+		$parameters = [
+			'image_id' => $image_id
+		];
+		$query->execute($parameters);
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		$url = $result['url'];
+		return $url;
 	}
-    public function addArticles(string $title, string $content, string $timestamp, int $imageArticleId) : void
-    {
-        $db=$this->db;
-        $query = $db->prepare('INSERT INTO blog (title, content, timestamp, image_id)
+	public function getAltByImageId(int $image_id): string
+	{
+		$db = $this->db;
+		$query = $db->prepare('SELECT alt FROM image_blog WHERE image_blog.id=:image_id');
+		$parameters = [
+			'image_id' => $image_id
+		];
+		$query->execute($parameters);
+		$result = $query->fetch(PDO::FETCH_ASSOC);
+		$alt = $result['alt'];
+		return $alt;
+	}
+	public function addArticles(string $title, string $content, string $timestamp, int $imageArticleId): void
+	{
+		$db = $this->db;
+		$query = $db->prepare('INSERT INTO blog (title, content, timestamp, image_id)
             VALUES (:title, :content, :timestamp, :imageArticleId)');
-        $parameters = [
-            'title' => $title,
-            'content' => $content,
-            'timestamp' => $timestamp,
-            'imageArticleId' => $imageArticleId
-        ];
-        $query->execute($parameters);
-        return;
-    }
-    public function addImageArticles(string $file_name,string $url,string $alt) : ?int
-    {
-        $db=$this->db;
-        $query = $db->prepare('INSERT INTO image_blog (file_name, url, alt)
-            VALUES (:file_name , :url, :alt)');
-        $parameters = [
-            'file_name' => $file_name,
-            'url' => $url,
-            'alt' => $alt
-        ];
-        $query->execute($parameters);
-        // on récupère l'id insérée
-        $id = $this->db->lastInsertId();
-        return $id;
-    }
-    public function editArticles(int $id, string $title, string $content, string $timestamp) : void
-    {
-        $db=$this->db;
-        $query = $db->prepare('UPDATE blog SET id=:id, title=:title, content=:content, timestamp=:timestamp WHERE id=:id');
-        $parameters = [
-            'id' => $id,
-            'title' => $title,
-            'content' => $content,
-            'timestamp' => $timestamp
-        ];
-        $query->execute($parameters);
-        return;
-    }
-    public function deleteArticleById(int $id): void
+		$parameters = [
+			'title' => $title,
+			'content' => $content,
+			'timestamp' => $timestamp,
+			'imageArticleId' => $imageArticleId
+		];
+		$query->execute($parameters);
+		return;
+	}
+	public function addImageArticles(string $file_name, string $url, string $alt): ?int
 	{
-		$db=$this->db;
+		$db = $this->db;
+		$query = $db->prepare('INSERT INTO image_blog (file_name, url, alt)
+            VALUES (:file_name , :url, :alt)');
+		$parameters = [
+			'file_name' => $file_name,
+			'url' => $url,
+			'alt' => $alt
+		];
+		$query->execute($parameters);
+		// on récupère l'id insérée
+		$id = $this->db->lastInsertId();
+		return $id;
+	}
+	public function editArticles(int $id, string $title, string $content, string $timestamp): void
+	{
+		$db = $this->db;
+		$query = $db->prepare('UPDATE blog SET id=:id, title=:title, content=:content, timestamp=:timestamp WHERE id=:id');
+		$parameters = [
+			'id' => $id,
+			'title' => $title,
+			'content' => $content,
+			'timestamp' => $timestamp
+		];
+		$query->execute($parameters);
+		return;
+	}
+	public function deleteArticleById(int $id): void
+	{
+		$db = $this->db;
 		$query = $db->prepare('DELETE FROM blog WHERE id=:id');
 		$parameters = [
 			'id' => $id
 		];
 		$query->execute($parameters);
-        
 	}
-    public function getLastNews() : array
+	public function getLastNews(): array
 	{
-			$db=$this->db;
-			$query = $db->prepare
-			('SELECT * FROM `blog`
+		$db = $this->db;
+		$query = $db->prepare(
+				'SELECT * FROM `blog`
 				ORDER BY `blog`.`id`  DESC
 				LIMIT 1'
 			);
-			$query->execute();
-			$lastNews = $query->fetchAll(PDO::FETCH_ASSOC);
-			return $lastNews;
+		$query->execute();
+		$lastNews = $query->fetchAll(PDO::FETCH_ASSOC);
+		return $lastNews;
 	}
 }
